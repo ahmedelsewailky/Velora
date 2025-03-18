095a2ecdc52790c8532459bbbbc270350d375f2d
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2025-03-19 01:15:51 +0200

    Header larg screen style completed

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,2 +1,2 @@
-$(function(){$("[data-bs-toggle='tooltip']").tooltip({trigger:"hover",placement:"bottom"})});
+$(function(){$("[data-bs-toggle='tooltip']").tooltip({trigger:"hover",placement:"bottom"}),$(document).mousemove(function(t){let e=t.clientX/window.innerWidth,i=t.clientY/window.innerHeight;$(".floating-img").each(function(){var t=$(this).data("speed"),n=(e-.5)*t*10,t=(i-.5)*t*10;$(this).css("transform",`translate(${n}px, ${t}px)`)})})});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,18 @@ $(function() {
         trigger: "hover",
         placement: "bottom"
     });
+
+    $(document).mousemove(function (event) {
+        let x = event.clientX / window.innerWidth;
+        let y = event.clientY / window.innerHeight;
+
+        $(".floating-img").each(function () {
+            let speed = $(this).data("speed");
+            let moveX = (x - 0.5) * speed * 10; 
+            let moveY = (y - 0.5) * speed * 10;
+
+            $(this).css("transform", `translate(${moveX}px, ${moveY}px)`);
+        });
+    });
+    
 });
