ce360c4c296003282551fc593b0ea4fa27b08662
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2025-03-27 09:46:42 +0200

    تحسينات في الريسبونسف

--- src/js/main.js
+++ src/js/main.js
@@ -10,23 +10,6 @@ $(function () {
     });
 
     /**
-     * Handles mouse movement to create a parallax effect at @page index.html [Hero Section].
-     * Elements with the class "abs" will move based on cursor position.
-     */
-    $(document).mousemove(function (event) {
-        let x = event.clientX / window.innerWidth;
-        let y = event.clientY / window.innerHeight;
-
-        $(".parallax-effect").each(function () {
-            let speed = $(this).data("speed");
-            let moveX = (x - 0.5) * speed * 10;
-            let moveY = (y - 0.5) * speed * 10;
-
-            $(this).css("transform", `translate(${moveX}px, ${moveY}px)`);
-        });
-    });
-
-    /**
      * Manages multi-level dropdowns.
      * Allows toggling nested dropdowns without closing the parent menu.
      */
